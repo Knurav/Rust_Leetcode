14d9980fc9f6a937a268ca585315292bfd0a8a48
Knurav <62768727+Knurav@users.noreply.github.com>
2023-08-06 22:20:30 +0530

    adds driver code

--- 217-contains-duplicate/contains_duplicate.rs
+++ 217-contains-duplicate/contains_duplicate.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+pub struct Solution;
+
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
         let mut map = HashSet::new();
@@ -13,5 +15,20 @@ impl Solution {
         }
 
         false
+    }
+}
+
+fn main() {
+    let test_cases = vec![
+        vec![1, 2, 3, 4, 5],
+        vec![1, 2, 3, 4, 1],
+        vec![1, 1, 1, 1],
+        vec![],
+        vec![10, 20, 30, 20],
+    ];
+
+    for nums in test_cases {
+        let result = Solution::contains_duplicate(nums.clone()); //To keep LC Function Sig
+        println!("Input: {:?} -> Contains Duplicate: {}", nums, result);
     }
 }
